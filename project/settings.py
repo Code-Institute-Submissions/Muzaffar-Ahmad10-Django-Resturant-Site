f7dfe367fc0c3c36239501a04ab7d8a6361da5dd
Muzaffar Ahmad Tahir <muzaffarahmad709@gmail.com>
2024-02-15 17:27:44 +0000

    add authentication

--- project/settings.py
+++ project/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-muzaffarahm-djangorestu-ws9pm69bepg.ws-eu108.gitpod.io',
 '.herokuapp.com']
@@ -42,9 +42,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
      'blog',
 
 ]
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -55,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'project.urls'
@@ -114,6 +123,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
--- project/urls.py
+++ project/urls.py
@@ -19,10 +19,10 @@ from django.urls import path, include
 from blog import views as blog_views
 
 urlpatterns = [
-    # path("blog/", blog_views.my_blog, name='blog'),
+    # path("blog/", blog_views.my_blog, nam e='blog'),
 
     path('admin/', admin.site.urls),
     path('', include('blog.urls'), name='blog_urls'),
-    
+    path("accounts/", include("allauth.urls")),
  
 ]
