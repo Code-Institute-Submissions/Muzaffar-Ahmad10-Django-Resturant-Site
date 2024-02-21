da753a25c0e41fb585bfa89274f50c468efb244b
Muzaffar Ahmad Tahir <muzaffarahmad709@gmail.com>
2024-02-21 12:52:28 +0000

    add some changes in models and views

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class book_tabel(models.Model):
     Name = models.CharField (max_length=55)
     Email = models.EmailField (max_length=55)
-    Number = models.IntegerField (max_length=25)
+    Number = models.IntegerField ()
     Date = models.DateField ()
-    Person = models.IntegerField (max_length=2)
+    Person = models.IntegerField ()
+    start_time = models.CharField (max_length=55)
+    end_time = models.CharField (max_length=55)
+    instructions = models.CharField (max_length=2000)
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from blog.models import book_tabel
 
+
+
 # Create your views here.
 
 
@@ -22,17 +24,31 @@ def signup(request):
 
 def reservation(request): 
     if request.method == 'POST':
+        print(request.POST)
         name = request.POST.get('name')
         email = request.POST.get('email')
         number = request.POST.get('number')
         date = request.POST.get('date')
-        person = request.POST.get('person')
+        person =  request.POST.get('person')
+        start_time = request.POST.get('start_time')
+        end_time = request.POST.get('end_time')
+        instructions = request.POST.get('instructions')
+        
+        
 
-        if name !='' and email !='' and number !='' and date !='' and person !='': 
+        if name !='' and email !='' and number !='' and date !='' and person !='':  
 
-            data = book_tabel(Name=name,Email=email,Number=number,Date=date,Person=person)
-            print(data)
-            data.save()
+            data = book_tabel.objects.create(
+                Name=name,
+                Email=email,
+                Number=number,
+                Date=date,
+                Person=person,
+                start_time=start_time,
+                end_time=end_time,
+                instructions=instructions)
+            # print(data)
+            # data.save()
  
 
-    return render(request,'reservation.html')        
+    return render(request,'reservation.html')     
