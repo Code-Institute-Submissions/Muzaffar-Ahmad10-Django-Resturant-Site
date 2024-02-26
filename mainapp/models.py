9f7067666d09e2ce86175eaa4f883efb8ad8f44d
Muzaffar Ahmad Tahir <muzaffarahmad709@gmail.com>
2024-02-26 16:02:50 +0000

    made model for the bookings and table reservation

--- mainapp/models.py
+++ mainapp/models.py
@@ -11,3 +11,19 @@ class ContactUs(models.Model):
 
     def __str__(self):
         return f"{self.name} - {self.email}"
+
+#Booking models for saving user bookings information
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    person_name = models.CharField(max_length=32, default="Anon person")
+    person_number = models.CharField(max_length=32, default=000)
+    person_email = models.EmailField(blank=True, null=True)
+    guest_count = models.CharField(max_length=32)
+    guest_table = models.CharField(max_length=32)
+    requested_date = models.DateField()
+    requested_time = models.TimeField()
+
+
+    def __str__(self):
+        return f"{self.user} - {self.person_name} - {self.requested_date} - {self.requested_time}" 
+
